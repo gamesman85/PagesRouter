ed0f66d128c07788e60ad36d3ef0cbd4d2fcf052
Daniel Wang <daniel.wang@try.no>
2024-05-10 16:13:52 +0200

    Weather component

---
+++ pages/components/weather.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import React, { useState, useEffect } from "react";
+
+const PageComponentWeather: NextPage = () => {
+    interface WeatherProps {
+        weather: string;
+    }
+
+    const WeatherComponent = (props: WeatherProps) => {
+        const [count, setCount] = useState(0);
+        useEffect(() => {
+            setCount(1);
+        }, []);
+
+        return (
+            <h1 onClick={() => setCount(count + 1)}>
+                The weather is {props.weather},
+                and the counter shows {count}
+            </h1>
+        );
+    };
+
+    return (
+        <div>
+            <WeatherComponent weather="sunny" />
+        </div>
+    );
+};
+
+export default PageComponentWeather;
